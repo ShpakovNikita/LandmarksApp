101b3f567147b894c620e3817aeffce57adfe60e
ShpakovNikita <shpakovnikita1998@gmail.com>
2020-12-18 12:01:46 -0500

    hike graph animations added

---
+++ LandmarksApp/Views/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  LandmarksApp
+//
+//  Created by Nikita Shpakov on 12/18/20.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
