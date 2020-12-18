ac3ca33ebb7f8724a020e5e12e89fbb5bca1045b
ShpakovNikita <shpakovnikita1998@gmail.com>
2020-12-18 12:41:36 -0500

    profile sheet added

---
+++ LandmarksApp/Models/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  LandmarksApp
+//
+//  Created by Nikita Shpakov on 12/18/20.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "n_shpakov")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
